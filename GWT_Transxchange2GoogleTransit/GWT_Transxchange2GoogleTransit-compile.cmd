cba95c971601d40fab3df3ca89e4a41e011a982f
Joachim.Pfeiffer <Joachim.Pfeiffer@705c8f7f-ad29-0410-9695-93dd3b14b492>
2007-03-16 20:43:12 +0000

    Rebuilding the source code
    git-svn-id: https://googletransitdatafeed.googlecode.com/svn/trunk@26 705c8f7f-ad29-0410-9695-93dd3b14b492

--- GWT_Transxchange2GoogleTransit/GWT_Transxchange2GoogleTransit-compile.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "%~dp0\src;%~dp0\bin;C:/cygwin/usr/src/gwt-windows-1.3.3/gwt-user.jar;C:/cygwin/usr/src/gwt-windows-1.3.3/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.GTDF.GWT_Transxchange2GoogleTransit
