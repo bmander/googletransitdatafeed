9a28ca50677b00690495f80748c8725e80b5234f
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-08 17:43:53 -0700

    dbload.py misc script

---
+++ python/misc/dbload.py
@@ -0,0 +1,12 @@
+from transitfeed import Loader
+import sys
+
+if len(sys.argv) < 3:
+  print "usage: script.py gtfs_filename database_filename"
+  exit()
+
+gtfs_filename = sys.argv[1]
+database_filename = sys.argv[2]
+
+loader = Loader( gtfs_filename, memory_db=False, db_filename=database_filename )
+sched = loader.Load()
